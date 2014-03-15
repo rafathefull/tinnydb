78f6437b422423389ea5ec9e8d4183de25e0fa34
rafathefull <rafa.thefull@gmail.com>
2014-03-15 10:20:35 +0100

    Howto connect signals from Glade

--- principal.py
+++ principal.py
@@ -5,6 +5,13 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
+class Handler():
+    def delete_event(self, widget,data=None):
+        print( "Call from Glade." )
+        # TIP:if you return 0 , destroy, but if you return 1, stop , not kill program
+        # TODO: Here create dialog ask if I want exit program
+
+        return 0
 
 class Principal:
     """
@@ -17,14 +24,20 @@ class Principal:
         self.cSql = None
         self.glade = glade
 
-        #accediendo a los controles
+        #Create object gtk.Window , from load window the glade
         self.window = self.glade.get_object('consultas')
+
+        # Example connect manual signal.
         self.window.connect("destroy", self.destroy)
+
+        # From Glade, signal delete_event the window consultas,  at class Handler.
+        self.glade.connect_signals( Handler() )
 
         self.window.show()
 
     # Salimos de la aplicacion
     def destroy(self, widget, data=None):
+        print( "Salgo de Aqui")
         gtk.main_quit()
 
     def main(self):
